ab41caf06800727d9cc111200bc631d9698c9f4e
Emily Ackerman <emilyackerman@bs-150-212-4-215.wireless.pitt.edu>
2020-10-28 14:56:57 -0400

    updating energy probability calculation for collaboration *not working*

--- working_mcmc_driver.py
+++ working_mcmc_driver.py
@@ -26,9 +26,10 @@ std5 =  1.2*np.genfromtxt('std_h1n1.csv', usecols = [1,2], delimiter = ',',
 # set initial conditions
 u0 = data1[0,] #DO I NEED TO HAVE TWO ICS? 
 par1 = np.array([78.6, 50.6, 11.6, 20.3, 29.8, 68.8])
+# par1 = np.array([195.29346766, 24.4311772, 5.35010129, 9.3532443, 98.41332147, 85.3461522 ])
 par5 = np.array([71.8, 52.6, 14.5, 21.1, 26.6, 67.9])
-upper = np.maximum(par1, par5)*1000
-lower = np.minimum(par1, par5)/1000
+upper = np.maximum(par1, par5)*10000
+lower = np.minimum(par1, par5)/10000
 
 
 def g(model, t, u0, p):
@@ -38,7 +39,7 @@ def g(model, t, u0, p):
 
 
 # initialize parameters
-beta = np.array([1.0, 0.5]) 
+beta = np.array([1.0]) 
 eps = 0.002/np.sqrt(beta) 
 nruns = 10000
 nchains = len(beta) 
@@ -56,15 +57,15 @@ pyh5n1 = np.zeros(np.shape(yh5n1)) #proposed pars
 
 
 Energy = np.zeros([nchains, nruns]) #Energy
-energyh1n1 = Energy #EnergyH1N1
-energyh5n1 = Energy
+energyh1n1 = np.zeros([nchains, nruns]) #EnergyH1N1
+energyh5n1 = np.zeros([nchains, nruns])
 e = np.zeros([nchains, 1]) #E
-eh1 = e #EH1
-eh5 = e
+eh1 = np.zeros([nchains, 1]) #EH1
+eh5 = np.zeros([nchains, 1])
 accept = np.zeros([nchains, 1]) 
-reject = accept
+reject = np.zeros([nchains, 1])
 accept_swap = np.zeros([nchains-1, 1])
-reject_swap = accept_swap
+reject_swap = np.zeros([nchains-1, 1])
 
 pars2change = np.array([0,1,2,3,4])    
 parsnotchanged = np.array([5])
@@ -73,10 +74,10 @@ for c in np.arange(0, nchains):
     print(c)
     tmax = 7
     tspan = np.arange(0,tmax+.05,0.05)
-    ychainh1n1[c, 0, :] = par1
-    yh1n1[c, :] = par1
-    ychainh5n1[c, 0, :] = par5
-    yh5n1[c, :] = par5
+    ychainh1n1[c, 0, :] = par1[:]
+    yh1n1[c, :] = par1[:]
+    ychainh5n1[c, 0, :] = par5[:]
+    yh5n1[c, :] = par5[:]
     sol = g(f2, tspan, u0, par1)
     energyh1n1[c, 0] = errorfxn(tvec, tspan, sol, data1, std1)
     sol = g(f2, tspan, u0, par5)
@@ -104,15 +105,14 @@ for run in np.arange(0, nruns-1):
 
         e[c] = eh1[c] + eh5[c]
 
-        delta = (-Energy[c, run+1] - e[c])
+        delta = (Energy[c, run+1] - e[c])
         h = np.min((1, np.exp(-beta[c]*delta)))
         rand_prob = np.random.random()
-        #print(rand_prob, h)
-        print(accept)
+        print(rand_prob, h, -beta[c]*delta, np.exp(-beta[c]*delta))
         if rand_prob < h:
             yh1n1[c,:] = pyh1n1[c,:]
+            yh5n1[c,:] = pyh5n1[c,:]
             accept[c] += 1
-            print(accept)
             Energy[c, run+1] = e[c]
             energyh1n1[c, run+1] = eh1[c]
             energyh5n1[c, run+1] = eh5[c]
@@ -129,9 +129,11 @@ for run in np.arange(0, nruns-1):
 
         if (run)%output == 0: 
             print("At iteration", run, "for chain temperature", beta[c])
-            print('chain', c, accept[c], reject[c])
             acceptance_rate = accept[c]/(accept[c]+reject[c])
             print("Acceptance rate = ", acceptance_rate)
+            print("accept: ", accept[c], " reject: ", reject[c])
             #swapping_rate =
             print("Min energy = ", np.min(Energy[c,0:run+1]))
             print("Max energy = ", np.max(Energy[c,0:run+1]))
+            print("H1N1", ychainh1n1[c, run+1, :])
+            print("H5N1", ychainh5n1[c, run+1, :], "\n")
