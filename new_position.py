9b11b57fdb504fd4aa6f4c504a45bc33ad58c254
Emily Ackerman <emilyackerman@bs-150-212-4-215.wireless.pitt.edu>
2020-10-30 09:48:59 -0400

    changed prior to uniform distribution

--- new_position.py
+++ new_position.py
@@ -3,12 +3,11 @@ import numpy as np
 def new_position(par, eps, upper, lower, ind):
 	npar = len(par)
 	new_par = par
-	for i in range(0,npar):
-		if i in ind:
-			scale = ((np.log(upper[i]) - np.log(lower[i])))/2
-			new_par[i] = par[i]*np.exp(eps*np.random.randn(1)*scale)
-			if (new_par[i] > upper[i]): 
-				new_par[i] = (lower[i]/upper[i])*new_par[i]
-			if new_par[i] < lower[i]: 
-				new_par[i] = (upper[i]/lower[i])*new_par[i]
+	for i in ind:
+		scale = ((np.log(upper[i]) - np.log(lower[i])))/2
+		new_par[i] = par[i]*np.exp(eps*np.random.uniform(0,1)*scale)
+		if (new_par[i] > upper[i]): 
+			new_par[i] = (lower[i]/upper[i])*new_par[i]
+		if new_par[i] < lower[i]: 
+			new_par[i] = (upper[i]/lower[i])*new_par[i]
 	return new_par
